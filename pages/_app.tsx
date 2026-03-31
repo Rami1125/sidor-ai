e79e9ec3b56154877524508562133dffab75c1d8
Rami1125 <rami.msarwa1@gmail.com>
2026-03-31 20:02:43 +0300

    Enhance app with PWA meta tags and manifest link
    Add meta tags and manifest link for PWA support

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,3 +4,7 @@ import type { AppProps } from 'next/app';
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
+<link rel="manifest" href="/manifest.json" />
+<meta name="apple-mobile-web-app-capable" content="yes" />
+<meta name="apple-mobile-web-app-status-bar-style" content="default" />
+<meta name="apple-mobile-web-app-title" content="sabanos" />
