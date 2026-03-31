c3c8cf01fd5aecd7952ccdc6a5cf647e9377c00c
Rami1125 <rami.msarwa1@gmail.com>
2026-03-31 20:06:21 +0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,16 +6,19 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        {/* זהות האפליקציה - sabanos */}
         <title>sabanos</title>
-        {/* הגדרות PWA להתקנה במסך מלא */}
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
+        
+        {/* הגדרות PWA להתקנה סלולרית */}
         <link rel="manifest" href="/manifest.json" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="sabanos" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
         <link rel="apple-touch-icon" href="https://i.postimg.cc/3wTMxG7W/ai.jpg" />
         <meta name="theme-color" content="#10b981" />
       </Head>
+      
       <Component {...pageProps} />
     </>
   );
