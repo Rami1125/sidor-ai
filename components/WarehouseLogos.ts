f8d451a431f0f7ba40b05f4ab97d3cc0119c0e35
Rami1125 <rami.msarwa1@gmail.com>
2026-04-02 06:07:54 +0300

    Create WarehouseLogos.ts

---
+++ components/WarehouseLogos.ts
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const logos = {
+  "שארק 30": "/logos/shark30.png",
+  "כראדי 32": "/logos/karadi32.png",
+  "שי שרון": "/logos/shai-sharon.png"
+};
+
+export default function WarehouseLogos(warehouse: string) {
+  const src = logos[warehouse];
+
+  if (!src) return null;
+
+  return (
+    <img
+      src={src}
+      className="w-full h-full object-contain"
+      alt={warehouse}
+    />
+  );
+}
