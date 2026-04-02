fbd59a92f88e3861e4e7bf76248a2aed42f8408e
Rami1125 <rami.msarwa1@gmail.com>
2026-04-02 06:28:39 +0300

    Update live.tsx

--- pages/admin/live.tsx
+++ pages/admin/live.tsx
@@ -3,6 +3,7 @@
 import React, { useState, useEffect } from 'react';
 import AppLayout from '../../components/Layout';
 import { supabase } from '../../lib/supabase';
+import ChatPopup from 'admin/ChatPopup';
 
 // אייקונים
 import {
